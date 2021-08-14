771be0d35578e7ed50feae9683654c7e9d02fdc5
victordantasdev <joaovictordjardim@gmail.com>
2021-08-14 06:04:58 -0300

    :fire: test

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import { ThemeContext } from 'styled-components';
 import Menu from '../src/components/commons/Menu';
 import Footer from '../src/components/commons/Footer';
