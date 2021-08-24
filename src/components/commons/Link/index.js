8dd70c02f93535d17f56aacb3b143bf36084d741
victordantasdev <joaovictordjardim@gmail.com>
2021-08-23 22:30:51 -0300

    feat(faq): adicionando faq dinamico

--- src/components/commons/Link/index.js
+++ src/components/commons/Link/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import NextLink from 'next/link';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import get from 'lodash/get';
 
 const StyledLink = styled.a`
-  color: red;
-  ${({ theme, color }) => (color
-    ? `color: ${get(theme, `colors.${color}.color`)}`
-    : 'color: inherit;')};
+  color: ${({ theme: { colorTheme } }) => colorTheme.tertiary.color};
   text-decoration: none;
   opacity: 1;
   transition: opacity ${({ theme }) => theme.transition};
