3355dbad2242fa664cfb2236511b5b9213106e1f
victordantasdev <joaovictordjardim@gmail.com>
2021-09-21 09:37:33 -0300

    feat(sobre): pagina sobre criada

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
+import AboutScreen from '../src/components/screen/AboutScreen';
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
-export default function Sobre() {
-  return <h1>Página Sobre</h1>;
+export async function getStaticProps() {
+  const DatoCMSURL = 'https://graphql.datocms.com/';
+
+  const client = new GraphQLClient(DatoCMSURL, {
+    headers: {
+      Authorization: `Bearer ${process.env.DATO_CMS_TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    query {
+      pageSobre {
+        pageTitle
+        pageDescription
+      }
+    }
+  `;
+
+  const messages = await client.request(query);
+
+  return {
+    props: {
+      messages,
+    },
+  };
 }
+
+export default websitePageHOC(AboutScreen);
