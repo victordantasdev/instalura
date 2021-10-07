32f709a21fa85f03a61356ceafff5a10a080cfe2
victordantasdev <joaovictordjardim@gmail.com>
2021-10-07 09:17:51 -0300

    fix(ci): fix 3 token

--- src/infra/cms/CMSGraphQLClient.js
+++ src/infra/cms/CMSGraphQLClient.js
@@ -2,19 +2,21 @@ import { GraphQLClient, gql as GraphQLTag } from 'graphql-request';
 
 export const gql = GraphQLTag;
 
-export function CMSGraphQLClient({ preview, datoCMSToken } = { preview: false }) {
+export function CMSGraphQLClient({ preview } = { preview: false }) {
   const DatoCMSURL = preview
-    ? 'https://graphql.datocms.com/'
-    : 'https://graphql.datocms.com/preview';
+    ? 'https://graphql.datocms.com/preview'
+    : 'https://graphql.datocms.com/';
+  const TOKEN = process.env.DATO_CMS_TOKEN;
   const client = new GraphQLClient(DatoCMSURL, {
     headers: {
-      Authorization: `Bearer ${datoCMSToken}`,
+      Authorization: `Bearer ${TOKEN}`,
     },
   });
 
   return {
     async query({ query, variables }) {
       const messages = await client.request(query, variables);
+
       return {
         data: {
           messages,
